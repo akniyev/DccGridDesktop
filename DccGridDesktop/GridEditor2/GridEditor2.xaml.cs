cc84e4c0f68042e17fd0a68c7773ff6b5f507a14
Hasan <hasan.akniyev@gmail.com>
2015-12-14 02:33:44 +0300

    added some checks and simple autodistribution

--- DccGridDesktop/GridEditor2/GridEditor2.xaml.cs
+++ DccGridDesktop/GridEditor2/GridEditor2.xaml.cs
@@ -137,6 +137,52 @@ namespace DccGridDesktop.GridEditor2
                 Rounds = new List<Heap<Participant>>();
             }
         }
+
+        public void AutoDistribute()
+        {
+            if (this.RoundId != -1) return;
+            if (this.participants.Count < 2) return;
+            if (this.BaseDistribution.Length < 2)
+            {
+                if (this.BaseDistribution.Length == 1 && this.BaseDistribution[0].Length >= this.participants.Count)
+                {
+                    for (int i = 0; i < this.participants.Count; i++)
+                    {
+                        this.BaseDistribution[0][i] = this.participants[i];
+                    }
+                    this.participants.Clear();
+                }
+                return;
+            }
+                
+
+            var count1 = 2;
+            for (int i = 0; i < this.BaseDistribution[0].Length; i++)
+            {
+                if (count1 / 2 + (participants.Count - count1) == this.BaseDistribution[1].Length)
+                {
+                    break;
+                } else
+                {
+                    count1++;
+                }
+            }
+
+            var count2 = participants.Count - count1;
+
+            for (int i = 0; i < count1; i++)
+            {
+                this.BaseDistribution[0][i] = participants[0];
+                participants.RemoveAt(0);
+            }
+
+            for (int i = 0; i < count2; i++)
+            {
+                this.BaseDistribution[1][i] = participants[0];
+                participants.RemoveAt(0);
+            }
+        }
+
         public List<Heap<Participant>> Rounds { get; set; }
 
         int ParticipantsCount
@@ -177,6 +223,76 @@ namespace DccGridDesktop.GridEditor2
 
         bool stopReloading = false;
 
+        List<string> isBaseDistributionCorrect(ParticipantGroup group)
+        {
+            var bd = group.BaseDistribution;
+            //Проверка, что все распределены
+            var result = new List<string>();
+            if (group.participants.Count > 0)
+            {
+                result.Add("Остались нераспределенные игроки!");
+            }
+
+            //Проверка, что разбивка по числу правильная
+            if (group.BaseDistribution.Length > 1)
+            {
+                int count1 = 0, count2 = 0;
+                count1 = group.BaseDistribution[0].Count(x => x != null);
+                count2 = group.BaseDistribution[1].Count(x => x != null);
+
+                if (!(count1 % 2 == 0 && (count1 / 2 + count2) == group.BaseDistribution[1].Length))
+                {
+                    result.Add("Количество игроков по раундам некорректно!");
+                }
+
+                for (int i = 0; i < group.BaseDistribution[0].Length / 2; i++)
+                {
+                    if (bd[0][2 * i] == null ^ bd[0][2 * i + 1] == null)
+                    {
+                        result.Add("Некорректное распределение по парам!");
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        List<string> isRoundDistributionCorrect(ParticipantGroup group)
+        {
+            var bd = group.Rounds[group.RoundId];
+            var bdr = bd[group.RoundId];
+            //Проверка, что все распределены
+            var result = new List<string>();
+
+            //Проверка, что разбивка по числу правильная
+            if (bd.Length > 1)
+            {
+                int count = 0;
+                for (int i =0; i < bd[group.RoundId].Length; i++)
+                {
+                    if (bd[group.RoundId][i] != null) count++;
+                }
+
+                if (count % 2 != 0)
+                {
+                    result.Add("Количество игроков в раунде некорректно!");
+                }
+
+                for (int i = 0; i < bd[group.RoundId].Length / 2; i++)
+                {
+                    if (bdr[2 * i] != null && bdr[2 * i + 1] != null)
+                    {
+                        if (!(bdr[2 * i].status != ParticipantStatus.Win ^ bdr[2 * i + 1].status != ParticipantStatus.Win))
+                        {
+                            result.Add("Некорректное распределение выигрышей!");
+                            break;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
         public void SetInfo (List<Participant> participants, List<WeightRange> weights, List<YearRange> years)
         {
             weights = weights.OrderBy(x => x.maxWeight).ToList();
@@ -194,6 +310,11 @@ namespace DccGridDesktop.GridEditor2
                 BaseDistribution = new Heap<Participant>(participants.Where(p => p.isInWeightRange(w) && p.isInYearRange(y)).ToList().Count),
                 RoundId = -1
             })).Where(g => g.participants.Count != 0).ToList();
+
+            foreach (var g in _groupedParticipants)
+            {
+                g.AutoDistribute();
+            }
         }
 
         void ReloadGui()
@@ -270,6 +391,8 @@ namespace DccGridDesktop.GridEditor2
                     if (roundId == col)
                     {
                         lblParticipant.MouseLeftButtonDown += markWinnerHandler;
+                        if (elem != null)
+                            lblParticipant.FontWeight = FontWeights.Bold;
                     }
 
                     if (elem != null && elem.status == ParticipantStatus.Win)
@@ -302,6 +425,24 @@ namespace DccGridDesktop.GridEditor2
             {
                 lblStatus.Content = "Раунд " + (_currentGroup.RoundId + 1);
             }
+
+            if (_currentGroup.RoundId == -1)
+            {
+                var errors = isBaseDistributionCorrect(_currentGroup);
+                if (errors.Count > 0)
+                {
+                    btnNext.IsEnabled = false;
+                }
+            }
+
+            if (_currentGroup.RoundId > -1)
+            {
+                var errors = isRoundDistributionCorrect(_currentGroup);
+                if (errors.Count > 0)
+                {
+                    btnNext.IsEnabled = false;
+                }
+            }
         }
 
         void markWinnerHandler(object sender, EventArgs e)
@@ -310,7 +451,10 @@ namespace DccGridDesktop.GridEditor2
             var col = label.col;
             var row = label.row;
 
+            var neibrow = (row / 2) * 2 + (1 - row % 2);
+
             var p = _currentGroup.Rounds[_currentGroup.RoundId][col][row];
+            var p_neib = _currentGroup.Rounds[_currentGroup.RoundId][col][neibrow];
 
             if (p == null)
                 return;
@@ -318,6 +462,11 @@ namespace DccGridDesktop.GridEditor2
             if (p.status != ParticipantStatus.Win)
             {
                 p.status = ParticipantStatus.Win;
+                
+                if (p_neib != null)
+                {
+                    p_neib.status = ParticipantStatus.None;
+                }
             }
             else
             {
@@ -504,6 +653,12 @@ namespace DccGridDesktop.GridEditor2
                 _currentGroup.Rounds.RemoveAt(_currentGroup.RoundId);
                 _currentGroup.RoundId--;
             }
+            LoadTree(_groupId, _currentGroup.RoundId);
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (_currentGroup == null) return;
             LoadTree(_groupId, _currentGroup.RoundId);
         }
     }
